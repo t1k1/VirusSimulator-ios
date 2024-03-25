182c5e3d442fe782cfc5ae5fe9682465e27073fd
Aleksei <ancientt1k1@gmail.com>
2024-03-25 15:32:13 +0300

    Имененна структура проекта, добавлены общие компоненты

--- VirusSimulator-ios/Common/Components/CustomLabel.swift
+++ VirusSimulator-ios/Common/Components/CustomLabel.swift
@@ -5,4 +5,24 @@
 //  Created by Aleksey Kolesnikov on 25.03.2024.
 //
 
-import Foundation
+import UIKit
+
+final class CustomLabel: UILabel {
+    init(text: String = "", fontSize: CGFloat = 20, numberOfLines: Int = 1) {
+        super.init(frame: .zero)
+        setupLabel(text: text, fontSize: fontSize, numberOfLines: numberOfLines)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLabel(text: String, fontSize: CGFloat, numberOfLines: Int) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        self.text = text
+        font = .systemFont(ofSize: fontSize)
+        self.numberOfLines = numberOfLines
+    }
+}
--- VirusSimulator-ios/Common/Components/CustomTextField.swift
+++ VirusSimulator-ios/Common/Components/CustomTextField.swift
@@ -5,4 +5,41 @@
 //  Created by Aleksey Kolesnikov on 25.03.2024.
 //
 
-import Foundation
+import UIKit
+
+final class CustomTextField: UITextField {
+    private let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        setupTextField(placeholder: placeholder)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.inset(by: padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.inset(by: padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.inset(by: padding)
+    }
+    
+    private func setupTextField(placeholder: String) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        font = .systemFont(ofSize: 20)
+        textColor = .black
+        self.placeholder = placeholder
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        
+    }
+}
